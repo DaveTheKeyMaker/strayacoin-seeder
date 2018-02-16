b115230ac42ca50b4a0e4b354b09d55dbaab7470
Jumail <mundekkat@hotmail.com>
2018-02-16 12:20:06 +1100

    Updated for Strayacoin

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb };
+unsigned char pchMessageStart[4] = { 0xd0, 0xe1, 0xf5, 0xec };
 
 CMessageHeader::CMessageHeader()
 {
